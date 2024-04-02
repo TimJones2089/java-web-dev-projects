a305d0f85a04fce7edf077923777de0fa7e976c9
Tim Jones <timjones.2089@gmail.com>
2024-04-01 19:51:49 -0500

    Area of Circle Studio

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/NumericTypes.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/NumericTypes.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class NumericTypes {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("Rectangle length:");
         Integer rectLength = input.nextInt();
+
         System.out.println("Rectangle width:");
         Integer rectWidth = input.nextInt();
+
         Integer rectArea = rectLength * rectWidth;
         System.out.println("The area of your rectangle is " + rectArea);
     }
